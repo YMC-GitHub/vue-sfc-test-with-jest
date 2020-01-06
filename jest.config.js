5bc29845c05c8afd3d52674225e7164ee676af7a
yemiancheng <ymc.github@gmail.com>
2020-01-06 09:18:11 +0800

    build:update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
+const path = require('path');
 module.exports = {
+    rootDir: path.resolve(__dirname, './'),
     "moduleFileExtensions": [
         "js",
         // tell Jest to handle `*.vue` files
@@ -6,7 +8,11 @@ module.exports = {
     ],
     "moduleNameMapper": {
         // support the same @ -> src alias mapping in source code
-        "^@/(.*)$": "<rootDir>/src/$1"
+        "^@/(.*)$": "<rootDir>/src/$1",
+        // support mock load static file
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+        // support mock load style file
+        '\\.(css|styl|less|sass|scss)$': '<rootDir>/_mocks__/styleMock.js'
     },
     "transform": {
         // process `*.js` files with `babel-jest`
@@ -17,5 +23,21 @@ module.exports = {
     "snapshotSerializers": [
         //for snapshot tests
         "<rootDir>/node_modules/jest-serializer-vue"
+    ],
+    //which file to test
+    testMatch: [
+        // match the test file
+        '<rootDir>/test/*.spec.js'
+    ],
+    testPathIgnorePatterns: [
+    ],
+    //where to save coverage test ouput
+    coverageDirectory: '<rootDir>/coverage',
+    //which file to coverage test
+    collectCoverageFrom: [
+        // 'src/components/**/*.(js|vue)',
+        'src/components/*.(vue)',
+        '!src/main.js',
+        '!**/node_modules/**'
     ]
 }
